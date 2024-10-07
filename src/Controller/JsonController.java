bae11898e51e8a4a69a596bdd6dfbeab609af062
DegaGit <deganaqualy@gmail.com>
2024-10-06 21:47:58 -0300

    Enxugando CÃódigo

--- src/Controller/JsonController.java
+++ src/Controller/JsonController.java
@@ -5,13 +5,11 @@
 package Controller;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,22 +25,11 @@ public class JsonController<T> {
         this.tipoClasse = tipoClasse;
         this.gson = new Gson();
     }
-
-
-    public List<T> readJsonToList(String caminhoArquivo) {
-        List<T> list = new ArrayList<>();
-        try {
-            String conteudoJson = new String(Files.readAllBytes(Paths.get(caminhoArquivo)));
-
-            Type tipoLista = TypeToken.getParameterized(List.class, tipoClasse).getType();
-
-            list = gson.fromJson(conteudoJson, tipoLista);
-        } catch (IOException e) {
-            System.err.println("Erro ao ler o arquivo Json: " + e.getMessage());
-        } catch (JsonSyntaxException e) {
-            System.err.println("Erro ao fozer o Parse do Json: " + e.getMessage());
-        }
-        return list;
+    
+    public List<T> readJsonToList(String caminhoArquivo) throws IOException {
+        String jsonString = Files.readString(Paths.get(caminhoArquivo));
+        Type listType = TypeToken.getParameterized(List.class, tipoClasse).getType();
+        return gson.fromJson(jsonString, listType);
     }
     
     public void writeToJsonFile(List<T> objetos, String caminhoArquivo) throws IOException {
